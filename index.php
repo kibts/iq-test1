8e29b14477115db590acac36dd69554970fb21a5
kibts <124083041+kibts@users.noreply.github.com>
2023-03-12 15:38:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KQT25T3');</script>
+})(window,document,'script','dataLayer','GTM-MP6M9FM');</script>
 <!-- End Google Tag Manager -->
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQT25T3"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MP6M9FM"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
